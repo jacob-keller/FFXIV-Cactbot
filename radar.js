99e9482e84eb04d09d37353de53787db658af056
Jacob Keller <jacob.keller@gmail.com>
2024-10-02 22:38:27 -0700

    radar: fix whitespace to align with cactbot style
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- radar.js
+++ radar.js
@@ -1,16 +1,16 @@
-// vim: et:ts=4:sw=4
+// vim: et:ts=2:sw=2
 
 Options.CustomMonsters = {
-    'Forlorn Maiden': {
-        'id': '',
-        'name': 'Forlorn Maiden',
-        'hp': 69814,
-        'rank': 'Fate',
-    },
-    'The Forlorn': {
-        'id': '',
-        'name': 'The Forlorn',
-        'hp': 431080,
-        'rank': 'Fate',
-    },
+  'Forlorn Maiden': {
+     'id': '',
+     'name': 'Forlorn Maiden',
+     'hp': 69814,
+     'rank': 'Fate',
+  },
+  'The Forlorn': {
+     'id': '',
+     'name': 'The Forlorn',
+     'hp': 431080,
+     'rank': 'Fate',
+  },
 };
