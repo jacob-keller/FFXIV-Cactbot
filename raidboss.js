8cff2111ac854537ef6f896cbf0a4290c60d645c
Jacob Keller <jacob.keller@gmail.com>
2024-10-17 18:27:37 -0700

    raidboss: remove unnecessary lines and braces
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- raidboss.js
+++ raidboss.js
@@ -10,9 +10,7 @@ Options.Triggers.push({
       },
       type: 'GameLog',
       netRegex: NetRegexes.message({ line: 'The smell of death has drawn a Forlorn maiden to the battlefield' }),
-      alertText: (data, matches, output) => {
-        return output.text();
-      },
+      alertText: (data, matches, output) => output.text(),
       outputStrings: {
         text: {
           en: 'Forlorn Maiden Appears',
@@ -26,9 +24,7 @@ Options.Triggers.push({
       },
       type: 'GameLog',
       netRegex: NetRegexes.message({ line: 'The smell of death has drawn the Forlorn to the battlefield' }),
-      alarmText: (data, matches, output) => {
-        return output.text();
-      },
+      alarmText: (data, matches, output) => output.text(),
       outputStrings: {
         text: {
           en: 'The Forlorn Appears',
@@ -42,9 +38,7 @@ Options.Triggers.push({
       },
       type: 'GameLog',
       netRegex: NetRegexes.message({ line: 'The minions of an extraordinarily powerful mark are on the hunt for prey' }),
-      alarmText: (data, matches, output) => {
-        return output.text();
-      },
+      alarmText: (data, matches, output) => output.text(),
       outputStrings: {
         text: {
           en: 'SS Hunt Adds',
